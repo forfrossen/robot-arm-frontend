3c521d8bf100dca3ab4bc56c0ff7e7c118945158
forfrossen <43250388+forfrossen@users.noreply.github.com>
2024-05-01 23:05:44 +0200

    stuff to make it work

--- app/components/servo-controller.tsx
+++ app/components/servo-controller.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Box, Typography } from "@mui/joy";
+import { Box, Input, Typography } from "@mui/joy";
 import Button from "@mui/joy/Button";
 import ToggleButtonGroup from "@mui/joy/ToggleButtonGroup";
 
@@ -12,28 +12,38 @@ type ServoControllerProps = {
 export const ServoController = (props: Readonly<ServoControllerProps>) => {
   const { handleValueChange, name } = props;
   const [value, setValue] = React.useState<string>("0");
-  const angles = ["0", "45", "90", "135", "180"];
-
+  const angles = ["0", "45", "90", "135", "180", "225"];
+  const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+  const valueChange = (event, newValue) => {
+    if (!newValue) return;
+    handleValueChange(newValue, name);
+    setValue(newValue);
+  };
   return (
     <Box mt={2}>
-      <Typography level="title-lg">Servo {name}</Typography>
-      <ToggleButtonGroup
-        value={value}
-        variant="soft"
-        onChange={(event, newValue) => {
-          if (!newValue) return;
-          handleValueChange(newValue, name);
-          setValue(newValue);
-        }}
-      >
-        {angles.map((angle) => {
-          return (
-            <Button key={angle} value={angle}>
-              {angle}
-            </Button>
-          );
-        })}
-      </ToggleButtonGroup>
+      <Typography level="title-lg">Servo {capitalizedName}</Typography>
+      <Box sx={{ display: "flex", flexDirection: "row" }}>
+        <ToggleButtonGroup value={value} variant="soft" onChange={valueChange} sx={{ mr: 2 }}>
+          {angles.map((angle) => {
+            return (
+              <Button key={angle} value={angle}>
+                {angle}
+              </Button>
+            );
+          })}
+        </ToggleButtonGroup>
+        <Button onClick={() => valueChange(undefined, String(Number(value) - 10))}>-10</Button>
+        <Input
+          placeholder="Type Angle in here…"
+          variant="outlined"
+          color="primary"
+          sx={{ width: 100 }}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          onBlur={(event) => valueChange(event, event.target.value)}
+        />
+        <Button onClick={() => valueChange(undefined, String(Number(value) + 10))}>+10</Button>
+      </Box>
     </Box>
   );
 };
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -12,7 +12,11 @@ import { usePageEffect } from "../core/page";
 
 export const Component = function Dashboard(): JSX.Element {
   usePageEffect({ title: "Dashboard" });
-  const socket = io("ws://localhost:3000", { autoConnect: false });
+  const socket = io("ws://localhost:3001", {
+    autoConnect: true,
+    ackTimeout: 10000,
+    retries: 3,
+  });
   const [isConnected, setIsConnected] = useState(socket.connected);
 
   useEffect(() => {
@@ -33,10 +37,11 @@ export const Component = function Dashboard(): JSX.Element {
     };
   }, []);
 
-  const servos = ["BaseTurner", "Shoulder", "Elbow", "Wrist", "WristTurner", "Gripper"];
+  const servos = ["baseTurner", "shoulder", "elbow", "wrist", "wristTurner", "gripper"];
 
   const valueChangeHandler = (value: string, name: string): void => {
     console.log(`Value of ${name} changed to ${value}`);
+    socket.emit(name, { pos: Number(value) });
   };
 
   return (
